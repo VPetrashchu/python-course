27cd4b69ea6f62a1d645637bccac00009988607d
Vasylyna Petraschuk <bridok1990@gmail.com>
2020-12-01 17:53:10 +0300

    add new files with hometasks

---
+++ task_1.py
@@ -0,0 +1,18 @@
+#name_user = input ("What is your name?")
+#age = int(input("How old are you?"))
+#city = input("Where do you live") 
+#print("Hello", name_user) 
+#print("Your age is ", age) 
+#print("You live in ", city) 
+ 
+
+num_a = int(input("Input first number?"))
+num_b = int(input("Input Second number?")) 
+print("a + b = ", num_a + num_b) 
+print("a - b = ", num_a - num_b) 
+print("a * b = ", num_a * num_b)
+print("a / b = ", num_a / num_b) 
+print("a ** b = ", num_a ** num_b) 
+
+#print(f{"a + b = ", num_a + num_b},{"a - b = ", num_a - num_b}, 
+#{"a * b = ", num_a * num_b},{"a / b = ", num_a / num_b},{"a ** b = ", num_a ** num_b})
---
+++ task_2.py
@@ -0,0 +1,8 @@
+num_a = int(input("Input first number?"))
+num_b = int(input("Input Second number?")) 
+print("a + b = ", num_a + num_b) 
+print("a - b = ", num_a - num_b) 
+print("a * b = ", num_a * num_b)
+print("a / b = ", num_a / num_b) 
+print("a ** b = ", num_a ** num_b) 
+
