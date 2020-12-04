9fa2840d7924ca36899b08dd6118b91a1501c8db
Vasylyna Zenin <=>
2020-12-04 18:38:35 +0300

    add 1st homework and 2nd homework

--- task_2.py
+++
@@ -1,8 +0,0 @@
-num_a = int(input("Input first number?"))
-num_b = int(input("Input Second number?")) 
-print("a + b = ", num_a + num_b) 
-print("a - b = ", num_a - num_b) 
-print("a * b = ", num_a * num_b)
-print("a / b = ", num_a / num_b) 
-print("a ** b = ", num_a ** num_b) 
-
